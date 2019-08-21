9a7b541c1b16797d77d61307ca90bee3bf4887b6
DoraLY <30978685+Ylarod@users.noreply.github.com>
2019-08-21 09:08:50 +0800

    Add files via upload

---
+++ Exam/2019/作业1-数学计算/F-1085-求两点间的距离.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)； //关闭scanf不可用的提示
+
+//以下为代码正文
+
+
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+   double x,y,a,b,c,d,e;
+   scanf ("%lf %lf %lf %lf",&a,&b,&c,&d);
+   x=c-a;
+   y=d-b;
+   e=pow(x,2)+pow(y,2);
+   e=sqrt(e);
+   printf("%g\n",e);
+}
